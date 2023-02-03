34f5f24aeb424f9780043f67f79f171971f3398d
Felipe Lerda <lerdafelipe@gmail.com>
2023-02-03 22:56:24 +0100

    some initial styles

--- src/app/components/Footer/Footer.jsx
+++ src/app/components/Footer/Footer.jsx
@@ -1,9 +1,30 @@
+import Link from 'next/link'
 import React from 'react'
+import styles from './footer.module.css'
 
 const Footer = () => {
   return (
-    <div>
-      <p>Politicas de privacidad.</p>
+    <div className={styles.footerContainer}>
+      <Link className={styles.footerPolitics} href='/'>Politicas de privacidad.</Link>
+      <footer className={styles.footer}>
+        <div className={styles.footerEffect}>
+          <span />
+        </div>
+        <div className={styles.footerContainersFlex}>
+          <div className={styles.div33}>
+            <Link className={styles.copyright} href='/'>© Rifamia</Link>
+          </div>
+          <div className={styles.div33}>
+            <Link className={styles.privacy} href='/'>Politicas de cookies y privacidad</Link>
+            <Link className={styles.terms} href='/'>Términos y condiciones</Link>
+          </div>
+          <div className={styles.div33}>
+            <Link href='/'>Wpp</Link>
+            <Link href='/'>Ig</Link>
+            <Link href='/'>@</Link>
+          </div>
+        </div>
+      </footer>
     </div>
   )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,6 @@
 // import { Inter } from '@next/font/google'
+import Image from 'next/image'
+import Link from 'next/link'
 import styles from './page.module.css'
 
 // const inter = Inter({ subsets: ['latin'] })
@@ -6,7 +8,34 @@ import styles from './page.module.css'
 export default function Home () {
   return (
     <main className={styles.main}>
-      <h1>Hola ¿Cómo estás? Bienvenid@ a rifamia</h1>
+      <h1>Sorteos Fáciles, Rápidos y Transparentes</h1>
+      <h4>Rifamia es una plataforma en la que podés armar tu rifa de una manera más estética, más cómoda, con resultados transparentes, integradas a un plataforma de pagos muy popular y confiable.</h4>
+      <div className={styles.buttonSection}>
+        <Link href='registro'>Crear cuenta</Link>
+        <Link href='login'>Ingresar a mi cuenta</Link>
+      </div>
+      <section className={styles.howCreateContainer}>
+        <h3>¿Cómo Funciona Rifamia?</h3>
+        <div className={styles.howWork}>
+          <div className={styles.div50}>
+            <Image width={401} height={367} alt='ilustración rifamia svg' src='/rifamia-home.svg' />
+          </div>
+          <article className={styles.div50}>
+            <div>
+              <h5>Crea cuenta</h5>
+              <p>Creas tu cuenta, rellenas tus datos personales para posibles contactos y finalmente podrás crear tus rifas personalizadas.</p>
+            </div>
+            <div>
+              <h5>Verificar cuenta</h5>
+              <p>Nos envías una solicitud de verificación, analizaremos tus datos para verificar tu cuenta y que tus rifas sean más confiables.</p>
+            </div>
+            <div>
+              <h5>Empezá a crear tus rifas</h5>
+              <p>Creas y configuras tus rifas de la manera que quieras, podes limitar la cantidad de números, precio del número, nombre de la rifa, premios, descripción, y muchas cosas más.</p>
+            </div>
+          </article>
+        </div>
+      </section>
     </main>
   )
 }
